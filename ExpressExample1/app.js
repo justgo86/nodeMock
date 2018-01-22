bf58540475448057b137a1200309840f9f3d8497
justgo86-PC\justgo86 <justgo86@naver.com>
2018-01-23 00:52:51 +0900

    170페이지까지 진행

---
+++ ExpressExample1/app.js
@@ -0,0 +1,12 @@
+var express = require('express'), http = require('http');
+
+//익스프레스 객체 생성
+var app = express();
+
+//기본 포트를 app 객체에 속성으로 설정
+app.set('port', process.env.PORT || 3000);
+
+//express 서버 시작
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("서버가 시작되었습니다. : "+ app.get('port'));
+});
