31d5badffe42a7c4dc93d97af003ce49fa0cb137
yonghun.kim <juistgo86@naver.com>
2019-04-21 22:16:42 +0900

    299

---
+++ DefaultExample/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    server_port: 3000,
+    db_url: 'mongodb://127.0.0.1:27017/local',
+    db_schemas: [
+        {
+            file: './user_schema',
+            collection: 'user3',
+            schemaName: 'UserSchema',
+            modelName: 'UserModel'
+        }
+    ],
+    route_info : [
+        {file : './user', path : '/process/log', method : 'login', type : 'post'},
+        {file : './user', path : '/process/adduser', method : 'adduser', type : 'post'},
+        {file : './user', path : '/process/listuser', method : 'listuser', type : 'post'}
+    ]
+
+}
